4516d46aae75de2081f1539028847028950f38ff
artemgon <artemhoncharuk123@gmail.com>
2025-06-08 11:36:24 +0300

    Project structure has been created. Next step is to fill files with code.

---
+++ BookLibrary.WPF/Views/CustomConfirmationDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookLibrary.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomConfirmationDialog.xaml
+    /// </summary>
+    public partial class CustomConfirmationDialog : Window
+    {
+        public CustomConfirmationDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
